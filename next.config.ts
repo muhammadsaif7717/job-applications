5dd10bd2fd28b9f5cae57daf4b4b9b0643c3d09f
MD. Saif Islam <muhammadsaif7717@gmail.com>
2026-03-18 09:13:40 +0600

    Added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = withPWA({
+  reactStrictMode: true,
+  // ✅ Disable Turbopack for Webpack PWA compatibility
+  experimental: {
+    turbo: false,
+  },
+});
 
 export default nextConfig;
